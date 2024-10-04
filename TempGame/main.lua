c29f01e79cff3351d3ba0c9cfa5e8bbb969cbbb3
MacGiv <55637723+MacGiv@users.noreply.github.com>
2024-10-03 22:45:59 -0300

    Add menu, in-game ui, sprites, win-defeat condition

--- TempGame/main.lua
+++ TempGame/main.lua
@@ -1,39 +1,61 @@
 
 function love.load()
+    myFont = love.graphics.newFont("res/nsecthin.ttf", 50)
+    gameState = "menu"
     createPlayer()
     createObstacles()
 end
 
 function love.update(dt)
-    updatePlayerMovement(dt, player)
-    updateObstacles(dt)
+    if gameState == "menu" then
+        updateMenu()
+    elseif gameState == "play" then
+        updatePlayerMovement(dt, player)
+        updateObstacles(dt)
 
-    -- Verificar colisiones del jugador con todos los obstáculos
-    for i, obs in ipairs(obstacles) do
-        if checkCollision(player, obs) and obs.isDestroyed == false then
-            player.lives = player.lives - 1
-            print("Player Lives: " .. player.lives)
-            obs.isDestroyed = true
+        -- Verificar colisiones y perder vidas
+        for i, obs in ipairs(obstacles) do
+            if checkCollision(player, obs) and not obs.isDestroyed then
+                player.lives = player.lives - 1
+                print("Player Lives: " .. player.lives)
+                obs.isDestroyed = true
+                if player.lives <= 0 then
+                    gameState = "gameover"
+                end
+            end
         end
-    end
 
+        if love.keyboard.isDown("escape") then
+            gameState = "gameover"
+        end
+
+    elseif gameState == "credits" then
+        if love.keyboard.isDown("space") then
+            gameState = "menu"
+        end
+    elseif gameState == "gameover" then
+        updateGameOver()
+    end
 end
 
 function love.draw()
-    -- Dibujar jugador
-    love.graphics.setColor(0.522, 0.914, 1, 1)
-    love.graphics.rectangle("line", player.posX, player.posY, player.width, player.height)
-    love.graphics.setColor(1,1,1,1)
+    love.graphics.setFont(myFont)
+    if gameState == "menu" then
+        drawMenu()
+    elseif gameState == "play" then
+        drawHUD()
+        drawPlayer()
     
-    -- Dibujar obstáculos
-    for i, obs in ipairs(obstacles) do
-        love.graphics.rectangle("fill", obs.x, obs.y, obs.width, obs.height)
+        -- Dibujar obstáculos
+        for i, obs in ipairs(obstacles) do
+            love.graphics.rectangle("fill", obs.x, obs.y, obs.width, obs.height)
+            love.graphics.draw(obs.image, obs.x, obs.y)
+        end
+    elseif gameState == "credits" then
+        drawCredits()
+    elseif gameState == "gameover" then
+        drawGameOver()
     end
-
-    -- Debug para el punto de anclaje del jugador
-    love.graphics.setColor(1,0,0,1)
-    love.graphics.circle("fill", player.posX, player.posY, 10)
-    love.graphics.setColor(1,1,1,1)
 end
 
 -- Crear el jugador
@@ -45,6 +67,8 @@ function createPlayer()
     player.height = 75
     player.width = 30
     player.lives = 3
+    player.score = 0
+    player.image = love.graphics.newImage("res/love_car.png")
 end
 
 -- Crear los obstáculos
@@ -58,7 +82,8 @@ function createObstacles()
             width = 50,
             height = 50,
             speed = math.random(200, 400),
-            isDestroyed = false
+            isDestroyed = false,
+            image = love.graphics.newImage("res/love_crate.png")
         })
     end
 end
@@ -73,6 +98,7 @@ function updateObstacles(dt)
             obs.y = math.random(-200, -50)
             obs.x = math.random(0, love.graphics.getWidth() - obs.width)
             obs.isDestroyed = false
+            player.score = player.score + 1
         end
     end
 end
@@ -80,7 +106,7 @@ end
 -- Actualizar movimiento del jugador
 function updatePlayerMovement(deltaTime, player)
     margin = 5
-    
+
     if love.keyboard.isDown("w") and player.posY > margin then
         player.posY = player.posY - player.speed * deltaTime
     end
@@ -95,10 +121,72 @@ function updatePlayerMovement(deltaTime, player)
     end
 end
 
--- Función de colisiones AABB
 function checkCollision(playerObj, obstacleObj)
     return playerObj.posX < obstacleObj.x + obstacleObj.width and
            playerObj.posX + playerObj.width > obstacleObj.x and
            playerObj.posY < obstacleObj.y + obstacleObj.height and
            playerObj.posY + playerObj.height > obstacleObj.y
+end
+
+function drawMenu()
+    screenPortion = love.graphics.getHeight() / 8
+    love.graphics.setColor(1, 0.733, 0, 1)
+    love.graphics.printf("BOX AVOIDER", 0, screenPortion, love.graphics.getWidth(), "center")
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.printf("Move with WASD and survive to get higher score!", 0.5, screenPortion*3, love.graphics.getWidth(), "center")
+    love.graphics.printf("1. Play", 0, screenPortion*5, love.graphics.getWidth(), "center")
+    love.graphics.printf("2. Credits", 0, screenPortion*6, love.graphics.getWidth(), "center")
+    love.graphics.printf("3. Exit", 0, screenPortion*7, love.graphics.getWidth(), "center")
+end
+
+function updateMenu()
+    if love.keyboard.isDown("1") then
+        gameState = "play"
+        print("Game State: Play")
+    elseif love.keyboard.isDown("2") then
+        gameState = "credits"
+        print("Game State: Credits")
+    elseif love.keyboard.isDown("3") then
+        love.event.quit()
+    end
+end
+
+function updateGameOver()
+    if love.keyboard.isDown("space") then
+        gameState = "menu"
+        player.lives = 3
+        player.score = 0
+        createObstacles()
+        print("Game State: Menu")
+    end
+end
+
+function drawGameOver()
+    love.graphics.printf("Game Over!", 0, 200, love.graphics.getWidth(), "center")
+    love.graphics.printf("Score: " .. (player.score or 0), 0, 300, love.graphics.getWidth(), "center")
+    love.graphics.printf("Press Space to go back to Menu", 0, 400, love.graphics.getWidth(), "center")
+end
+
+function drawHUD()
+    love.graphics.print("Lives: " .. player.lives, 10, 10)
+    love.graphics.print("Score: " .. math.floor(player.score), love.graphics.getWidth() - 400, 10)
+end
+
+function drawPlayer()
+    playerSpriteMargin = 10
+    love.graphics.draw(player.image, player.posX-player.width, player.posY - playerSpriteMargin, 0, 1.5, 1.5)
+    love.graphics.rectangle("line", player.posX, player.posY, player.width, player.height)
+end
+
+function drawCredits()
+    screenPortion = love.graphics.getHeight() / 8
+
+    love.graphics.setColor(1, 0.733, 0, 1)
+    love.graphics.printf("Credits", 0, screenPortion, love.graphics.getWidth(), "center")
+    love.graphics.setColor(1,1,1,1)
+    love.graphics.printf("<< TP Bonus LOVE >>", 0, screenPortion*2, love.graphics.getWidth(), "center")
+    love.graphics.printf("Tomas Francisco Luchelli", 0, screenPortion*3, love.graphics.getWidth(), "center")
+    love.graphics.printf("Practica Profesional 1:", 0, screenPortion*4, love.graphics.getWidth(), "center") 
+    love.graphics.printf("Desarrollo de Videojuegos 1", 0, screenPortion*5, love.graphics.getWidth(), "center")
+    love.graphics.printf("Press SPACE to go back", 0, screenPortion*6, love.graphics.getWidth(), "center")
 end
